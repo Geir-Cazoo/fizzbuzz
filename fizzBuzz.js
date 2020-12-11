af8109040c431a0b81d94e7ba15ee7b7acb19330
Geir Gulbrandsen <geir.gulbrandsen@cazoo.co.uk>
2020-12-11 16:12:11 +0000

    Extracted divisability check

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,9 +1,13 @@
 
 function fizzBuzz(number) {
 
-    if ((number % 3 === 0) && (number % 5 === 0)) return 'fizzbuzz';
-    if (number % 3 === 0) return 'fizz';
-    if (number % 5 === 0) return 'buzz';
+    function isDivisableBy(number, x) {
+        return number % x === 0;
+    }
+
+    if (isDivisableBy(number, 3) && isDivisableBy(number,5)) return 'fizzbuzz';
+    if (isDivisableBy(number, 3)) return 'fizz';
+    if (isDivisableBy(number, 5)) return 'buzz';
     return number.toString();
 }
 
