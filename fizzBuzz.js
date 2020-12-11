02e04df1da002c3b1ab1ca6e21f1c5b0a8687385
Tim Gane <tim.gane@focalstrategy.com>
2020-12-11 15:22:09 +0000

    Broke the test

---
+++ fizzBuzz.js
@@ -0,0 +1,6 @@
+
+function fizzBuzz() {
+    return 1;
+}
+
+module.exports = fizzBuzz;
--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,5 +1,11 @@
+const fizzBuzz = require('./fizzBuzz');
+
 describe('FizzBuzz', () => {
     it('should retur 1 when passed 1', () => {
         expect(fizzBuzz(1)).toBe(1);
+    });
+
+    it('should return 2 when passed 2', () => {
+        expect(fizzBuzz(2)).toBe(2);
     });
 })
