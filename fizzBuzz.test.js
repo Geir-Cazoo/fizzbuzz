b8124965769e6f91335593a84faa8d0ce5c29c28
Geir Gulbrandsen <geir.gulbrandsen@cazoo.co.uk>
2020-12-11 15:57:40 +0000

    Now parameterising things

--- fizzBuzz.test.js
+++ fizzBuzz.test.js
@@ -1,47 +1,22 @@
 const fizzBuzz = require('./fizzBuzz');
 
-describe('FizzBuzz', () => {
-    it('should retur 1 when passed 1', () => {
-        expect(fizzBuzz(1)).toBe('1');
-    });
-
-    it('should return 2 when passed 2', () => {
-        expect(fizzBuzz(2)).toBe('2');
-    });
-
-    it('should return fizz when passed 3', () => {
-        expect(fizzBuzz(3)).toBe('fizz');
-    });
-
-    it('should return buzz when passed 5', () => {
-        expect(fizzBuzz(5)).toBe('buzz');
-    });
-
-    it('should return fizz when passed 6', () => {
-        expect(fizzBuzz(6)).toBe('fizz');
-    });
-
-    it('should return fizz when passed 9', () => {
-        expect(fizzBuzz(9)).toBe('fizz');
-    });
-
-    it('should return buzz when passed 10', () => {
-        expect(fizzBuzz(10)).toBe('buzz');
-    });
-
-    it('should return buzz when passed 20', () => {
-        expect(fizzBuzz(20)).toBe('buzz');
-    });
-
-    it('should return 11 when passed 11', () => {
-        expect(fizzBuzz(11)).toBe('11');
-    });
-
-    it('should return fizzbuzz when passed 15', () => {
-        expect(fizzBuzz(15)).toBe('fizzbuzz');
-    });
-
-    it('should return fizzbuzz when passed 60', () => {
-        expect(fizzBuzz(60)).toBe('fizzbuzz');
+describe.each([
+    [3, 'fizz'],
+    [6, 'fizz'],
+    [9, 'fizz'],
+    [5, 'buzz'],
+    [10, 'buzz'],
+    [20, 'buzz'],
+    [15, 'fizzbuzz'],
+    [30, 'fizzbuzz'],
+    [45, 'fizzbuzz'],
+    [2, '2'],
+    [7, '7'],
+    [22, '22'],
+])('FizzBuzz', (number, expected) => {
+    it(`should return ${expected} when passed ${number}`, () => {
+        expect(fizzBuzz(number)).toBe(expected);
     });
 })
+
+
