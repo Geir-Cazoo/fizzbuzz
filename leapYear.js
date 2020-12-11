01bcb9e1a027385b76db9506ecf68c3d4b1cf336
Geir Gulbrandsen <geir.gulbrandsen@cazoo.co.uk>
2020-12-11 16:55:25 +0000

    400 is a leapyear

--- leapYear.js
+++ leapYear.js
@@ -2,6 +2,7 @@
 function leapYear(year) {
     if(year === 4) return true;
     if(year === 8) return true;
+    if(year === 400) return true;
     if(year % 100 === 0) return false;
 
     return false;
