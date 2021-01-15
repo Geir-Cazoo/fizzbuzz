28838c88d71110b4d023079488c4ac1b88dbcf7a
MikePMichael <michael@focalstrategy.com>
2021-01-15 16:25:33 +0000

    mob next [ci-skip]

--- tictactoe.test.js
+++ tictactoe.test.js
@@ -1,11 +1,25 @@
 class TicTacToe {
+    constructor() {
+        this.board = [];
+    }
+
     status() {
         return 'empty board'
     }
 
-    play() {
-        return 'valid move'
+    play(piece, pos) {
+        if (this.board.length == 0) {
+            this.board[pos] = piece;
+            return 'valid move'
+        }
+
+        if (this.board[pos]) {
+            return 'space taken';
+        }
+
     }
+
+
 }
 
 describe("TicTacToe", () => {
@@ -13,6 +27,16 @@ describe("TicTacToe", () => {
         expect(new TicTacToe().status()).toBe('empty board');
     });
     it("should return 'valid move' for move X1", () => {
-        expect(new TicTacToe().play('X',1)).toBe('valid move');
+        expect(new TicTacToe().play('X', 1)).toBe('valid move');
     });
+    it("should return 'invalid turn' for move O1", () => {
+        expect(new TicTacToe().play('O', 1)).toBe('invalid turn');
+    });
+
+    it("should return 'space taken' for move X1", () => {
+        let game = new TicTacToe();
+        game.play('X', 1);
+        expect(game.play('X', 1)).toBe('space taken');
+    });
+
 });
